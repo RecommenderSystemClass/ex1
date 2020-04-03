924e088b29b69da3fe4845f23899583bab3e085d
gshamay <gshamay@gmail.com>
2020-04-03 18:43:07 +0300

    Update SVD.py

--- ex1/SVD.py
+++ ex1/SVD.py
@@ -12,40 +12,48 @@ import sys
 import time
 import math
 import pandas as pd
+import pickle
 
 seed = 80
 random.seed(seed)
 
-trainData = './data/trainData.csv'
-# trainData = "D:/BGU/RS/EXs/ex1/ex1/data/trainData.csv"  # used this line for console debug
-trainDataDF = load(trainData)
-# todo: split train validations 30% of the users
-# todo: split select validations lines to validate only on 30% of the products
+# trainData = './data/trainData.csv'
+trainData = "D:/BGU/RS/EXs/ex1/ex1/data/trainData.csv"  # used this line for console debug
+trainDataDF_all = load(trainData)
+trainDataDF_all.isnull().values.any()  # todo: check validity of the data
 
-trainDataDF = trainDataDF.sample(frac=1).reset_index(drop=True)  # shuffle train Data
-trainDataDF.isnull().values.any()  # todo: check validity of the data
+# split train data to train and validations - take 30% of the users
+# then select 30% of the samples of those users to be the train data and all teh rest is teh test data
+products = trainDataDF_all['business_id'].unique()
+users = trainDataDF_all['user_id'].unique()
+np.random.shuffle(users)
+userSplit = int(len(users) * 0.7)
+train_users, validation_users = users[:userSplit], users[userSplit:]
+validationUsersData = trainDataDF_all.loc[trainDataDF_all['user_id'].isin(validation_users)]
+validationDataDF = validationUsersData.sample(frac=0.3,
+                                              random_state=seed)  # todo: Should we take here 30% of the product that the validations users use or 30% randomally  ?
+trainDataDF = trainDataDF_all.drop(validationDataDF.index)
+trainDataDF = trainDataDF.sample(frac=1).reset_index(drop=True)  # shuffle train Data #todo: Check if really needed
 
-users = trainDataDF['user_id'].unique()
-products = trainDataDF['business_id'].unique()
+trainProducts = trainDataDF['business_id'].unique()
+trainUsers = trainDataDF['user_id'].unique()
 
 indexUsers = 0
-dicUsers = {}
-for user in users:
-    dicUsers[user] = indexUsers
+trainUsersDic = {}
+for user in trainUsers:
+    trainUsersDic[user] = indexUsers
     indexUsers += 1
 
 indexProducts = 0
-dicProducts = {}
-for product in products:
-    dicProducts[product] = indexProducts
+trainProductsDic = {}
+for product in trainProducts:
+    trainProductsDic[product] = indexProducts
     indexProducts += 1
 
 K = 100  # todo check values 100-500
 lam = 0.05  # regularization #todo: learn this value in X validations
 delta = 0.05  # learning rate #todo: learn this value
 mu = trainDataDF['stars'].mean()
-
-
 Bu = {}
 Bi = {}
 P = {}
@@ -58,6 +66,12 @@ for product in products:
     Q[product] = (np.random.rand(K) * 0.5 - 0.25)
     Bi[product] = np.random.rand() * 0.5 - 0.25
 
+with open('.\data\P.dump', 'wb') as fp:
+    pickle.dump(P, fp)
+
+with open('.\data\P.dump', 'rb') as fp:
+    PP = pickle.load(fp)
+
 
 ###########################################################
 def E(u, i):
@@ -68,25 +82,19 @@ def R(u, i):
     return mu + Bi[i] + Bu[u] + P[u].dot(Q[i])
 
 
-def handleRatingLine(user_id, business_id, stars, idx):
-    u = user_id
-    i = business_id
-    q = Q[i]
-    p = P[u]
-    bu = Bu[u]
-    bi = Bi[i]
+def handleRatingLine(user_id, business_id, stars):
+    q = Q[business_id]
+    p = P[user_id]
+    bu = Bu[user_id]
+    bi = Bi[business_id]
     Rui = mu + bi + bu + p.dot(q)
     currentCalculatedRates.append(
         Rui)  # todo: this will not be needed when calculating RMSE will be calc on validaiton set
     Eui = stars - Rui
-    newQi = q + delta * (Eui * p - lam * q)
-    newPu = p + delta * (Eui * q - lam * p)
-    newBu = bu + delta * (Eui - lam * bu)
-    newBi = bi + delta * (Eui - lam * bi)
-    Q[i] = newQi
-    P[u] = newPu
-    Bu[u] = newBu
-    Bi[i] = newBi
+    Q[business_id] = q + delta * (Eui * p - lam * q)
+    P[user_id] = p + delta * (Eui * q - lam * p)
+    Bu[user_id] = bu + delta * (Eui - lam * bu)
+    Bi[business_id] = bi + delta * (Eui - lam * bi)
 
 
 actualRates = trainDataDF['stars'].to_list()
@@ -113,7 +121,8 @@ while currentRMSE < lastRMSE:
     # keep last P and Q and use them -->keep the one with better error rate
     lastP = P
     lastQ = Q
-    trainDataDF.apply(lambda x: handleRatingLine(x['user_id'], x['business_id'], x['stars'], x.name), axis=1)
+
+    trainDataDF.apply(lambda x: handleRatingLine(x['user_id'], x['business_id'], x['stars']), axis=1)
     lastRMSE = currentRMSE
     # todo: run Predict on the validation set and get RMSE
     # todo: add additional methion, other then RMSE
