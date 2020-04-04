fc115b1bd44ce522f8364fa5c3a75f69b2dcc13c
gshamay <gshamay@gmail.com>
2020-04-04 18:04:32 +0300

    SVD MULTIPLE TEST

--- ex1/SVD.py
+++ ex1/SVD.py
@@ -33,7 +33,7 @@ userSplit = int(len(users) * 0.7)
 train_users, validation_users = users[:userSplit], users[userSplit:]
 validationUsersData = trainDataDF_all.loc[trainDataDF_all['user_id'].isin(validation_users)]
 validationDataDF = validationUsersData.sample(
-    frac=0.3,#take 30% of the data of the usres we selected for training
+    frac=0.3,  # take 30% of the data of the usres we selected for training
     random_state=seed)  # todo: Should we take here 30% of the product that the validations users use or 30% randomally  ?
 trainDataDF = trainDataDF_all.drop(validationDataDF.index)
 trainDataDF = trainDataDF.sample(frac=1).reset_index(drop=True)  # shuffle train Data #todo: Check if really needed
@@ -61,147 +61,169 @@ trainProductsDic = {}
 for product in trainProducts:
     trainProductsDic[product] = indexProducts
     indexProducts += 1
-
-K = 100  # todo check values 100-500
-lam = 0.05  # regularization #todo: learn this value in X validations
-delta = 0.05  # learning rate #todo: learn this value
+BInitialValuePlusMinusIntervals = [0.1]  # [0.005, 0.01, 0.05, 0.1, 0.25, 0.5, 0.75, 1]
+PQInitialValuePlusMinusIntervals = [0.1]  # [0.005, 0.01, 0.05, 0.1, 0.25, 0.5, 0.75, 1]
+Ks = [100]  # [100, 200, 300, 400, 500]
+lams = [0.07]      # [0.01, 0.02, 0.05, 0.07]    # regularization
+deltas = [0.05]    # [0.01, 0.02, 0.05, 0.07]   # learning rate
 mu = trainDataDF['stars'].mean()
-for K in [100, 200, 300, 400, 500]:
-    Bu = {}
-    Bi = {}
-    P = {}
-    for user in users:
-        P[user] = (np.random.rand(K) * 0.5 - 0.25)
-        Bu[user] = np.random.rand() * 0.5 - 0.25
 
-    Q = {}
-    for product in products:
-        Q[product] = (np.random.rand(K) * 0.5 - 0.25)
-        Bi[product] = np.random.rand() * 0.5 - 0.25
+for K in Ks:
+    for PQInitialValuePlusMinusInterval in PQInitialValuePlusMinusIntervals:
+        for BInitialValuePlusMinusInterval in BInitialValuePlusMinusIntervals:
+            for lam in lams:
+                for delta in deltas:
+                    Bu = {}
+                    Bi = {}
+                    P = {}
+                    for user in users:
+                        P[user] = (np.random.rand(K) * (
+                                    PQInitialValuePlusMinusInterval * 2) - PQInitialValuePlusMinusInterval)
+                        Bu[user] = np.random.rand() * (
+                                    BInitialValuePlusMinusInterval * 2) - BInitialValuePlusMinusInterval
 
-
-    ###########################################################
-
-    def calculateSingleRate(ratingLine):
-        u = ratingLine['user_id']
-        i = ratingLine['business_id']
-        return mu + Bi[i] + Bu[u] + P[u].dot(Q[i])
-
-
-    def handleRatingLine(ratingLine):
-        user_id = ratingLine['user_id']
-        business_id = ratingLine['business_id']
-        stars = ratingLine['stars']
-        q = Q[business_id]
-        p = P[user_id]
-        bu = Bu[user_id]
-        bi = Bi[business_id]
-        Rui = mu + bi + bu + p.dot(q)  # R(u, i)
-        Eui = stars - Rui
-        Q[business_id] = q + delta * (Eui * p - lam * q)
-        P[user_id] = p + delta * (Eui * q - lam * p)
-        Bu[user_id] = bu + delta * (Eui - lam * bu)
-        Bi[business_id] = bi + delta * (Eui - lam * bi)
+                    Q = {}
+                    for product in products:
+                        Q[product] = (
+                                np.random.rand(K) * (
+                                    PQInitialValuePlusMinusInterval * 2) - PQInitialValuePlusMinusInterval)
+                        Bi[product] = np.random.rand() * (
+                                    BInitialValuePlusMinusInterval * 2) - BInitialValuePlusMinusInterval
 
 
-    actualRates = validationDataDF['stars'].to_list()
-    iterations = 0
-    currentRMSE = sys.maxsize - 1
-    lastRMSE = sys.maxsize
+                    ###########################################################
 
-    beginTime = time.time()
-    print("Beggining: "
-          + "K[" + str(K) + "]"
-          + "lambda[" + str(lam) + "]"
-          + "delta[" + str(delta) + "]"
-          + "mu[" + str(mu) + "]"
-          + "P len[" + str(len(P)) + "]"
-          + "Q len [" + str(len(Q)) + "]"
-          )
-
-    lastP = []
-    lastQ = []
+                    def calculateSingleRate(ratingLine):
+                        u = ratingLine['user_id']
+                        i = ratingLine['business_id']
+                        return mu + Bi[i] + Bu[u] + P[u].dot(Q[i])
 
 
-    def predictRates(newDataFrame):
-        return newDataFrame.apply(calculateSingleRate, axis=1).tolist()
+                    def handleRatingLine(ratingLine):
+                        user_id = ratingLine['user_id']
+                        business_id = ratingLine['business_id']
+                        stars = ratingLine['stars']
+                        q = Q[business_id]
+                        p = P[user_id]
+                        bu = Bu[user_id]
+                        bi = Bi[business_id]
+                        Rui = mu + bi + bu + p.dot(q)  # R(u, i)
+                        Eui = stars - Rui
+                        Q[business_id] = q + delta * (Eui * p - lam * q)
+                        P[user_id] = p + delta * (Eui * q - lam * p)
+                        Bu[user_id] = bu + delta * (Eui - lam * bu)
+                        Bi[business_id] = bi + delta * (Eui - lam * bi)
 
 
-    while currentRMSE < lastRMSE:
-        iterationBeginTime = time.time()
-        iterations += 1
-        # keep last P and Q and use them -->keep the one with better error rate
-        lastP = P
-        lastQ = Q
-        trainDataDF.apply(handleRatingLine, axis=1)
-        currentCalculatedRates = predictRates(validationDataDF)
-        lastRMSE = currentRMSE
-        # todo: add additional method, other then RMSE
-        currentRMSE = RMSE(currentCalculatedRates, actualRates)
-        print("lastRMSE [" + str(lastRMSE) + "]"
-              + "currentRMSE[" + str(currentRMSE) + "]"
-              + "iterations[" + str(iterations) + "]"
-              + "SecIter[" + str(time.time() - iterationBeginTime) + "]"
-              + "SecBegin[" + str(time.time() - beginTime) + "]"
-              )
+                    actualRates = validationDataDF['stars'].to_list()
+                    iterations = 0
+                    currentRMSE = sys.maxsize - 1
+                    lastRMSE = sys.maxsize
 
-    P = lastP
-    Q = lastQ
-    learningTime = time.time() - beginTime
+                    beginTime = time.time()
+                    print("Beggining: "
+                          + "K[" + str(K) + "]"
+                          + "lambda[" + str(lam) + "]"
+                          + "delta[" + str(delta) + "]"
+                          + "PQInitialInterval[+-" + str(PQInitialValuePlusMinusInterval) + "]"
+                          + "BInitialInterval[+-" + str(BInitialValuePlusMinusInterval) + "]"
+                          + "mu[" + str(mu) + "]"
+                          + "P len[" + str(len(P)) + "]"
+                          + "Q len [" + str(len(Q)) + "]"
+                          )
+
+                    lastP = []
+                    lastQ = []
 
 
-    class mySvd:
-        def __init__(mysillyobject,
-                     p,
-                     q,
-                     Bu,
-                     Bi,
-                     mu,
-                     rmse):
-            mysillyobject.p = p
-            mysillyobject.q = q
-            mysillyobject.Bi = Bi
-            mysillyobject.Bu = Bu
-            mysillyobject.mu = mu
-            mysillyobject.rmse = rmse
+                    def predictRates(newDataFrame):
+                        return newDataFrame.apply(calculateSingleRate, axis=1).tolist()
 
 
-    mySvdSave = mySvd(P, Q, Bu, Bi, mu, lastRMSE)
-    filePath = '.\data\K' + str(K) + '_lam' + str(lam) + '_delta' + str(delta) + '.dump'
-    with open(filePath, 'wb') as fp:
-        pickle.dump(mySvdSave, fp)
+                    while currentRMSE < lastRMSE:
+                        iterationBeginTime = time.time()
+                        iterations += 1
+                        # keep last P and Q and use them -->keep the one with better error rate
+                        lastP = P
+                        lastQ = Q
+                        trainDataDF.apply(handleRatingLine, axis=1)
+                        currentCalculatedRates = predictRates(validationDataDF)
+                        lastRMSE = currentRMSE
+                        # todo: add additional method, other then RMSE
+                        currentRMSE = RMSE(currentCalculatedRates, actualRates)
+                        print("lastRMSE [" + str(lastRMSE) + "]"
+                              + "currentRMSE[" + str(currentRMSE) + "]"
+                              + "iterations[" + str(iterations) + "]"
+                              + "SecIter[" + str(time.time() - iterationBeginTime) + "]"
+                              + "SecBegin[" + str(time.time() - beginTime) + "]"
+                              )
 
-    # mySvdload = None
-    # with open(filePath, 'rb') as fp:
-    #     mySvdload = pickle.load(fp)
+                    P = lastP
+                    Q = lastQ
+                    learningTime = time.time() - beginTime
 
-    predictBeginTime = time.time()
-    calculatedTestRates = predictRates(testDataDF)
-    PredictTime = time.time() - predictBeginTime
-    actuaTestRaes = testDataDF['stars'].to_list()
-    testRMSE = RMSE(actuaTestRaes, calculatedTestRates)
 
-    print("********************************************************")
-    print("SVD "
-          + "RMSE on Test[" + str(testRMSE) + "]"
-          + "RMSE on Train[" + str(lastRMSE) + "]"
-          + "learningTime[" + str(learningTime) + "]"
-          + "PredictTime[" + str(PredictTime) + "]"
-          + "K[" + str(K) + "]"
-          + "lambda[" + str(lam) + "]"
-          + "delta[" + str(delta) + "]"
-          + "mu[" + str(mu) + "]"
-          + "users[" + str(len(P)) + "]"
-          + "trainAndValidaiton Size[" + str(len(trainDataDF_all)) + "]"
-          + "train Size[" + str(len(trainDataDF)) + "]"
-          + "validation Size[" + str(len(validationDataDF)) + "]"
-          + "test Orig Size[" + str(len(testDataDF_orig)) + "]"
-          + "Test Size[" + str(len(testDataDF)) + "]"
-          + "missingUsers[" + str(len(missingUsers)) + "]"
-          + "missingProducts[" + str(len(missingProducts)) + "]"
-          + "learn iterations[" + iterations + "]"
-          )
-    print("********************************************************")
+                    class mySvd:
+                        def __init__(mysillyobject,
+                                     p,
+                                     q,
+                                     Bu,
+                                     Bi,
+                                     mu,
+                                     rmse):
+                            mysillyobject.p = p
+                            mysillyobject.q = q
+                            mysillyobject.Bi = Bi
+                            mysillyobject.Bu = Bu
+                            mysillyobject.mu = mu
+                            mysillyobject.rmse = rmse
+
+
+                    predictBeginTime = time.time()
+                    calculatedTestRates = predictRates(testDataDF)
+                    PredictTime = time.time() - predictBeginTime
+                    actuaTestRaes = testDataDF['stars'].to_list()
+                    testRMSE = RMSE(actuaTestRaes, calculatedTestRates)
+
+                    mySvdSave = mySvd(P, Q, Bu, Bi, mu, lastRMSE)
+                    filePath = '.\data\K' + str(K) \
+                               + '_lam' + str(lam) \
+                               + '_delta' + str(delta) \
+                               + '_PQ' + str(PQInitialValuePlusMinusInterval) \
+                               + '_B' + str(BInitialValuePlusMinusInterval) \
+                               + '_RMSE' + str(testRMSE) \
+                               + '.dump'
+
+                    with open(filePath, 'wb') as fp:
+                        pickle.dump(mySvdSave, fp)
+
+                    # mySvdload = None
+                    # with open(filePath, 'rb') as fp:
+                    #     mySvdload = pickle.load(fp)
+
+                    print("********************************************************")
+                    print("SVD "
+                          + "RMSE on Test[" + str(testRMSE) + "]"
+                          + "RMSE on Train[" + str(lastRMSE) + "]"
+                          + "learningTime[" + str(learningTime) + "]"
+                          + "PredictTime[" + str(PredictTime) + "]"
+                          + "K[" + str(K) + "]"
+                          + "lambda[" + str(lam) + "]"
+                          + "delta[" + str(delta) + "]"
+                          + "mu[" + str(mu) + "]"
+                          + "PQInitialInterval[+-" + str(PQInitialValuePlusMinusInterval) + "]"
+                          + "BInitialInterval[+-" + str(BInitialValuePlusMinusInterval) + "]"
+                          + "users[" + str(len(P)) + "]"
+                          + "trainAndValidaiton Size[" + str(len(trainDataDF_all)) + "]"
+                          + "train Size[" + str(len(trainDataDF)) + "]"
+                          + "validation Size[" + str(len(validationDataDF)) + "]"
+                          + "test Orig Size[" + str(len(testDataDF_orig)) + "]"
+                          + "Test Size[" + str(len(testDataDF)) + "]"
+                          + "missingUsers[" + str(len(missingUsers)) + "]"
+                          + "missingProducts[" + str(len(missingProducts)) + "]"
+                          + "learn iterations[" + str(iterations) + "]"
+                          )
+                    print("********************************************************")
 
 print(" ---- Done ---- ")
 exit(0)
