3f1ef8fd2501a60f446158a96c91ad6bbb4a1f81
alexd <alex.daniels85@gmail.com>
2020-04-12 18:40:48 +0300

    changed names of classes

--- ex1/RecommenderSystem.py
+++ ex1/RecommenderSystem.py
@@ -1,103 +1,272 @@
-import time
-import numpy as np
-import pandas as pd
-from category_encoders import TargetEncoder
-import nltk
-nltk.download('vader_lexicon')
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
+# Alex Danieli 317618718
+# Gil Shamay 033076324
+
+# requirements:
+#   python3 x64,
+#   pandas,
+#   numpy,
+#   surprise,
+#   nltk,
+#   sklearn,
+#   category-encoders
+#   train and data and test are expected to be @ './data/trainData.csv' and './data/testData.csv' respectively
+
+
+from builtins import print
 from RMSE import *
 from MAE import *
 from printDebug import *
 from load import *
+from SVD import *
+from SentimentBasedModel import *
+import numpy as np
+import random
+import time
+import sys
+import math
+import pandas as pd
+import pickle
+import socket
+
+seed = 80
+random.seed(seed)
+#################################
+# Parameters
+SVDppOptions = [False]  # SVD++
+Ks = [400]  # 400 # [100, 200, 300, 400, 500]
+deltas = [0.03]  # 0.03 # [0.03, 0.04, 0.05, 0.06, 0.07]  # learning rate
+lams = [0.07]  # 0.07 # [0.03, 0.04, 0.05, 0.06, 0.07]  # regularization
+errorCalculation = 'RMSE'  # select the error calculation method used to build the SVD / SVD++ model with
+# errorCalculation = 'MAE'
+ensamble = True
+
+# initial random values range for the different matrix
+BInitialValuePlusMinusIntervals = [0.01]  # [0.005, 0.01, 0.05, 0.1, 0.25, 0.5, 0.75, 1]
+PQInitialValuePlusMinusIntervals = [0.01]  # [0.005, 0.01, 0.05, 0.1, 0.25, 0.5, 0.75, 1]
+YInitialValueInterval = 0.01
+
+#################################
+# The Data
+trainData = './data/trainData.csv'
+testData = './data/testData.csv'
 
 
-def prepare_data():
-    df_train = load('./data/trainData.csv')
-    df_train = df_train.dropna()
-    df_test = load('./data/testData.csv')
-    df_test = df_test.dropna()
-    x, y_train, y_test = prepareDataForSemantic(df_train, df_test)
-    return x, y_train, y_test
+################################
+# Helpers
+################################
+def cleanDataFromNulls(data):
+    len1 = len(data)
+    data.isnull().values.any()  # check validity of the data
+    data = data.dropna()
+    len2 = len(data)
+    print("removed [" + str(len2 - len1) + "]null data entries")
+    return data
 
 
-def prepareDataForSemantic(df_train, df_test):
-    x_train = df_train[['user_id', 'text']]
-    y_train = df_train['stars']
-    x_test = df_test[['user_id', 'text']]
-    y_test = df_test['stars']
-    x = pd.concat([x_train, x_test], keys=['train', 'test'])
-    return x, y_train, y_test
+def splitTrainValidation(trainDataDF_all):
+    #   split train data to train and validations - take 30% of the users
+    #   then select 30% of the samples of those users to be the train data and all teh rest is teh test data
+    users = trainDataDF_all['user_id'].unique()
+    np.random.shuffle(users)
+    userSplit = int(len(users) * 0.7)
+    validation_users = users[userSplit:]
+    validationUsersData = trainDataDF_all.loc[trainDataDF_all['user_id'].isin(validation_users)]
+    validationDataDF = validationUsersData.sample(
+        frac=0.3,  # take 30% of the data of the usres we selected for training
+        random_state=seed)  # we take here 30% of the validaitons users ratings randomally
+    trainDataDF = trainDataDF_all.drop(validationDataDF.index)
+    trainDataDF = trainDataDF.sample(frac=1).reset_index(drop=True)  # shuffle train Data
+    trainProducts = trainDataDF['business_id'].unique()
+    trainUsers = trainDataDF['user_id'].unique()
+    # Clean validations from cold start
+    validationDataDF, missingProducts, missingUsers = cleanColdStarts(trainProducts, trainUsers, validationDataDF)
+    return validationDataDF, trainDataDF, trainProducts, trainUsers
 
 
-def split_and_reduce(x):
-    x_train = x.loc['train']
-    x_test = x.loc['test']
-    x_train = x_train[['neutral', 'positive', 'text_size', 'user_id']]
-    x_test = x_test[['neutral', 'positive', 'text_size', 'user_id']]
-    return x_train, x_test
+def cleanColdStarts(trainProducts, trainUsers, data_orig):
+    # clean validaiton data - remove all entries with new users or product (no cold start)
+    retData = data_orig.loc[data_orig['user_id'].isin(trainUsers)]
+    retData = retData.loc[retData['business_id'].isin(trainProducts)]
+    dataFilteredOut = data_orig.drop(retData.index)
+    missingProducts = dataFilteredOut['business_id'].unique()
+    missingUsers = dataFilteredOut['user_id'].unique()
+    return retData, missingProducts, missingUsers
 
 
-def score_to_sentiment(score):
-    if score >= 0.3:
-        return 'positive'
-    elif score <= -0.3:
-        return 'negative'
-    return 'neutral'
+def trainSVDorSVDpp():
+    global mySvd
+    printDebug("***** train SVD or SVDpp *****")
+    mySvd = SVD(trainDataDF,
+                validationDataDF,
+                K,  # = 400
+                lam,  # = 0.07
+                delta,  # = 0.03
+                errorCalculation,  # = 'RMSE' / 'MAE'
+                PQInitialValuePlusMinusInterval,  # = 0.01
+                BInitialValuePlusMinusInterval,  # = 0.01
+                SVDpp,  # = False/True
+                lam2,  # = 0.07
+                YInitialValueInterval  # = 0.01
+                )
 
 
-def extract_sentiment(row, analyzer):
-    all_scores = analyzer.polarity_scores(row['text'])
-    score = all_scores['compound']
-    return score_to_sentiment(score)
+def predictSVD():
+    global calculatedTestRates, PredictTime
+    printDebug("***** Predict SVD/SVD++ on Test *****")
+    predictBeginTime = time.time()
+    calculatedTestRates = mySvd.predictRates(testDataDF)
+    PredictTime = time.time() - predictBeginTime
 
 
-def extract_features(df):
-    analyzer = SentimentIntensityAnalyzer()
-    df['sentiment'] = df.apply(lambda row: extract_sentiment(row, analyzer), axis = 1)
-    df['neutral'] = np.where(df['sentiment'] != 'negative', 1, 0)
-    df['positive'] = np.where(df['sentiment'] == 'positive', 1, 0)
-    df['text_size'] = df.apply(lambda row: len(row['text']), axis = 1)
-    return df
-
-
-def train_classifier(x_train, training_labels):
-    print("Train LinearRegression")
-    model = LinearRegression()
-    model.fit(x_train, np.array(training_labels))
-    training_predictions = model.predict(x_train)
-    print("Model coefficients are: " + str(model.coef_))
-    print("Model intercept is: " + str(model.intercept_))
-    print('Training RMSE score: {}'.format(RMSE(training_labels, training_predictions)))
-    print('Training MAE score: {}'.format(MAE(training_labels, training_predictions)))
-    return model
-
-
-def test_classifier(classifier, x_test, testing_labels):
-    print("Test LinearRegression")
-    testing_predictions = classifier.predict(x_test)
-    print('Testing RMSE score: {}'.format(RMSE(testing_labels, testing_predictions)))
-    print('Testing MAE score: {}'.format(MAE(testing_labels, testing_predictions)))
-
-
-def apply_target_encoder(x_train, x_test, y_train, column, smoothing_param):
-    encoder = TargetEncoder(cols=[column], return_df=True, smoothing=smoothing_param)
-    x_train = encoder.fit_transform(x_train, y_train)
-    x_test = encoder.transform(x_test)
-    return x_train, x_test
-
-
-def runSemantic():
-    beginTime = time.time()
-    x, y_train, y_test = prepare_data()
-    ###########################
+def trainSentiment():
+    global x_test, classifier
+    printDebug("***** build sentiment prediciton *****")
+    # build Sentiment Predictor using the same tarin data
+    x, y_train, y_test = prepareDataForSemantic(trainDataDF_all, testDataDF)
     x = extract_features(x)
     x_train, x_test = split_and_reduce(x)
     x_train, x_test = apply_target_encoder(x_train, x_test, y_train, 'user_id', 1)
     classifier = train_classifier(x_train, y_train)
-    test_classifier(classifier, x_test, y_test)
-    print("time=" + str(time.time() - beginTime))
 
-# runSemantic()
+
+def predictSentiment():
+    global sentimentModelRatesPrediction
+    printDebug("***** Predict using the semantic Predictor on Test *****")
+    sentimentModelRatesPrediction = classifier.predict(x_test)
+
+
+def trainCombinedModel():
+    trainSVDorSVDpp()
+    trainSentiment()
+
+
+def predictCombinedModel():
+    global ensamblePrediciton
+    predictSVD()
+    predictSentiment()
+    printDebug("***** calc Ensamble *****")
+    ensamblePrediciton = (sentimentModelRatesPrediction + calculatedTestRates) / 2
+
+
+def calculatErrors():
+    global testRMSE, testMAE, ensambleStringResults
+    ######## Calc Errors  ########
+    actuaTestRates = testDataDF['stars'].to_list()
+    testRMSE = RMSE(actuaTestRates, calculatedTestRates)
+    testMAE = MAE(actuaTestRates, calculatedTestRates)
+    testSentimentRMSE = None
+    testSentimentMAE = None
+    testEnsambleRMSE = None
+    testEnsambleMAE = None
+    ensambleStringResults = ""
+    if (ensamble):
+        testSentimentRMSE = RMSE(actuaTestRates, sentimentModelRatesPrediction)
+        testSentimentMAE = MAE(actuaTestRates, sentimentModelRatesPrediction)
+        testEnsambleRMSE = RMSE(actuaTestRates, ensamblePrediciton)
+        testEnsambleMAE = MAE(actuaTestRates, ensamblePrediciton)
+        ensambleStringResults = "S_RMSE[" + str("%.5f" % testSentimentRMSE) + "]" \
+                                + "S_MAE[" + str("%.5f" % testSentimentMAE) + "]" \
+                                + "EnRMSE[" + str("%.5f" % testEnsambleRMSE) + "]" \
+                                + "EnMAE[" + str("%.5f" % testEnsambleMAE) + "]"
+
+
+def printResults():
+    ######## Print results and save log to file  ########
+    printDebug("********************************************************")
+    strFinalResult = ("SVD "
+                      + errorCalculation + "OnTrain[" + str(mySvd.lastError) + "]"
+                      + "RMSEOnTest[" + str(testRMSE) + "]"
+                      + "MAEOnTest[" + str(testMAE) + "]"
+                      + "K[" + str(K) + "]"
+                      + "lambda[" + str(lam) + "]"
+                      + "delta[" + str(delta) + "]"
+                      + "mu[" + str(mySvd.mu) + "]"
+                      + "learningTime[" + str(mySvd.learningTime) + "]"
+                      + "PredictTime[" + str(PredictTime) + "]"
+                      + "PQInitialInterval[+-" + str(PQInitialValuePlusMinusInterval) + "]"
+                      + "BInitialInterval[+-" + str(BInitialValuePlusMinusInterval) + "]"
+                      + "users[" + str(len(mySvd.P)) + "]"
+                      + "trainAndValidaiton Size[" + str(len(trainDataDF_all)) + "]"
+                      + "train Size[" + str(len(trainDataDF)) + "]"
+                      + "validation Size[" + str(len(validationDataDF)) + "]"
+                      + "test Orig Size[" + str(len(testDataDF_orig)) + "]"
+                      + "Test Size[" + str(len(testDataDF)) + "]"
+                      + "missingUsers[" + str(len(missingUsers)) + "]"
+                      + "missingProducts[" + str(len(missingProducts)) + "]"
+                      + "learn iterations[" + str(mySvd.iterations) + "]"
+                      + "SVDpp[" + str(SVDpp) + "]"
+                      + "YInitialValuePlusMinusInterval[" + str(YInitialValueInterval) + "]"
+                      + 'host[' + socket.gethostname() + "]"
+                      + ensambleStringResults
+                      )
+    printDebug(strFinalResult)
+    printDebug("********************************************************")
+    fileBaseName = errorCalculation + "Train[" + str("%.5f" % mySvd.lastError) + "]" \
+                   + "RMSEOnTest[" + str("%.5f" % testRMSE) + "]" \
+                   + "MAEOnTest[" + str("%.5f" % testMAE) + "]" \
+                   + "K[" + str(K) + "]" \
+                   + "lambda[" + str(lam) + "]" \
+                   + "delta[" + str(delta) + "]" \
+                   + "learnSec[" + str("%.2f" % mySvd.learningTime) + "]" \
+                   + "PredictSec[" + str("%.2f" % PredictTime) + "]" \
+                   + "SVDpp[" + str(SVDpp) + "]" \
+                   + ensambleStringResults \
+                   + 'host[' + socket.gethostname() + "]"
+    printToFile(".\\results\\" + fileBaseName + ".log")
+
+
+################################
+# load train data
+trainDataDF_all = load(trainData)
+trainDataDF_all = cleanDataFromNulls(trainDataDF_all)
+validationDataDF, trainDataDF, trainProducts, trainUsers = splitTrainValidation(trainDataDF_all)
+################################
+# load test data
+testDataDF_orig = load(testData)
+# Clean test from cold start
+testDataDF, missingProducts, missingUsers = cleanColdStarts(trainProducts, trainUsers, testDataDF_orig)
+printDebug("testDataDF after clean size[" + str(len(testDataDF))
+           + "]testDataDF_orig[" + str(len(testDataDF_orig)) + "]")
+
+
+def saveModelToFile():
+    pass
+    ##########################################
+    # Save the Model to be reused if needed
+    # dumpFileFullPath = ".\\dumps\\" + fileBaseName + ".dump"
+    # with open(dumpFileFullPath, 'wb') as fp:
+    #     pickle.dump(mySvd, fp)
+    ##########################################
+    # Load the Model to be reused - sample code
+    # mySvdload = None
+    # with open(dumpFileFullPath, 'rb') as fp:
+    #     mySvdload = pickle.load(fp)
+
+
+################################
+# run
+# We add an option to run a few options - to have multiple parameters tests
+# this was used to select the best lambda, Dalta and K
+# (we also tried a few different ranges for the P, Q, Y random values matrix
+for SVDpp in SVDppOptions:
+    for K in Ks:
+        for PQInitialValuePlusMinusInterval in PQInitialValuePlusMinusIntervals:
+            for BInitialValuePlusMinusInterval in BInitialValuePlusMinusIntervals:
+                for lam in lams:
+                    lam2 = lam  # used for SVD++; can get a different value then lam
+                    for delta in deltas:
+                        printDebug(" ---- Build Model and Predict ---- ")
+                        if (ensamble):
+                            trainCombinedModel()
+                            predictCombinedModel()
+                        else:
+                            trainSVDorSVDpp()
+                            predictSVD()
+
+                        printDebug(" ---- Calculate Errors and Print  ---- ")
+                        calculatErrors()
+                        printResults()
+                        saveModelToFile()
+
+printDebug(" ---- Done ---- ")
+exit(0)
---
+++ ex1/SentimentBasedModel.py
@@ -0,0 +1,103 @@
+import time
+import numpy as np
+import pandas as pd
+from category_encoders import TargetEncoder
+import nltk
+nltk.download('vader_lexicon')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from RMSE import *
+from MAE import *
+from printDebug import *
+from load import *
+
+
+def prepare_data():
+    df_train = load('./data/trainData.csv')
+    df_train = df_train.dropna()
+    df_test = load('./data/testData.csv')
+    df_test = df_test.dropna()
+    x, y_train, y_test = prepareDataForSemantic(df_train, df_test)
+    return x, y_train, y_test
+
+
+def prepareDataForSemantic(df_train, df_test):
+    x_train = df_train[['user_id', 'text']]
+    y_train = df_train['stars']
+    x_test = df_test[['user_id', 'text']]
+    y_test = df_test['stars']
+    x = pd.concat([x_train, x_test], keys=['train', 'test'])
+    return x, y_train, y_test
+
+
+def split_and_reduce(x):
+    x_train = x.loc['train']
+    x_test = x.loc['test']
+    x_train = x_train[['neutral', 'positive', 'text_size', 'user_id']]
+    x_test = x_test[['neutral', 'positive', 'text_size', 'user_id']]
+    return x_train, x_test
+
+
+def score_to_sentiment(score):
+    if score >= 0.3:
+        return 'positive'
+    elif score <= -0.3:
+        return 'negative'
+    return 'neutral'
+
+
+def extract_sentiment(row, analyzer):
+    all_scores = analyzer.polarity_scores(row['text'])
+    score = all_scores['compound']
+    return score_to_sentiment(score)
+
+
+def extract_features(df):
+    analyzer = SentimentIntensityAnalyzer()
+    df['sentiment'] = df.apply(lambda row: extract_sentiment(row, analyzer), axis = 1)
+    df['neutral'] = np.where(df['sentiment'] != 'negative', 1, 0)
+    df['positive'] = np.where(df['sentiment'] == 'positive', 1, 0)
+    df['text_size'] = df.apply(lambda row: len(row['text']), axis = 1)
+    return df
+
+
+def train_classifier(x_train, training_labels):
+    print("Train LinearRegression")
+    model = LinearRegression()
+    model.fit(x_train, np.array(training_labels))
+    training_predictions = model.predict(x_train)
+    print("Model coefficients are: " + str(model.coef_))
+    print("Model intercept is: " + str(model.intercept_))
+    print('Training RMSE score: {}'.format(RMSE(training_labels, training_predictions)))
+    print('Training MAE score: {}'.format(MAE(training_labels, training_predictions)))
+    return model
+
+
+def test_classifier(classifier, x_test, testing_labels):
+    print("Test LinearRegression")
+    testing_predictions = classifier.predict(x_test)
+    print('Testing RMSE score: {}'.format(RMSE(testing_labels, testing_predictions)))
+    print('Testing MAE score: {}'.format(MAE(testing_labels, testing_predictions)))
+
+
+def apply_target_encoder(x_train, x_test, y_train, column, smoothing_param):
+    encoder = TargetEncoder(cols=[column], return_df=True, smoothing=smoothing_param)
+    x_train = encoder.fit_transform(x_train, y_train)
+    x_test = encoder.transform(x_test)
+    return x_train, x_test
+
+
+def runSemantic():
+    beginTime = time.time()
+    x, y_train, y_test = prepare_data()
+    ###########################
+    x = extract_features(x)
+    x_train, x_test = split_and_reduce(x)
+    x_train, x_test = apply_target_encoder(x_train, x_test, y_train, 'user_id', 1)
+    classifier = train_classifier(x_train, y_train)
+    test_classifier(classifier, x_test, y_test)
+    print("time=" + str(time.time() - beginTime))
+
+# runSemantic()
